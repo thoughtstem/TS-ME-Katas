4b6809ad227b1b73a4fa22cc457b96e489eb58d2
sonnynajar <sonny.najar@gmail.com>
2019-12-20 07:40:30 -0800

    Update read-code-stimuli.rkt

--- ts-cutscene/katas/read-code-stimuli.rkt
+++ ts-cutscene/katas/read-code-stimuli.rkt
@@ -13,12 +13,27 @@
    'hello-world-1
    (read "Code a basic cutscene.")
 
-
-   ;Text Stimuli
+   ; --- Text Stimuli
    'text-1
-   (read-prefix "that has one page with text.")
+   (read-prefix "that has big text and purple text.")
    'text-2
-   (read-prefix "that has two pages one with text moving to the right, the other one with text moving to the left.")
+   (read-prefix "that has red text and italized text.")
    'text-3
+   (read-prefix "that has small, slanted text.")
+
+   ; --- Page Stimuli
+   'page-1
+   (read-prefix "that has a page with an orange background.")
+   'page-2
+   (read-prefix "that has a page with a blue border.")
+   'page-3
+   (read-prefix "that has a large space between the lines of text.")
+   
+   ; --- Mode Stimuli
+   'mode-1
+   (read-prefix "that has one page with text.")
+   'mode-2
+   (read-prefix "that has two pages one with text moving to the right, the other one with text moving to the left.")
+   'mode-3
    (read-prefix "that has two pages one with text moving up, the other one with text moving down.")
    ))
