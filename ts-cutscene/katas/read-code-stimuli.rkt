648f31c5f4152b0a46cd02d8bc422dfb29b58b27
sonnynajar <sonny.najar@gmail.com>
2019-12-30 11:34:25 -0800

    stimuli and tips

--- ts-cutscene/katas/read-code-stimuli.rkt
+++ ts-cutscene/katas/read-code-stimuli.rkt
@@ -20,6 +20,10 @@
    (read-prefix "that has red text and italized text.")
    'text-3
    (read-prefix "that has small, slanted text.")
+   'text-4
+   (read-prefix "that has a font with a custom family and weight.")
+   'text-5
+   (read-prefix "where each line of text is different.")
 
    ; --- Page Stimuli
    'page-1
@@ -28,12 +32,28 @@
    (read-prefix "that has a page with a blue border.")
    'page-3
    (read-prefix "that has a large space between the lines of text.")
+   'page-4
+   (read-prefix "that has an image as the background.")
+   'page-5
+   (read-prefix "that has a small page in the top left corner.")
+   'page-6
+   (read-prefix "that has 3 pages, each with their own duration.")
+   'page-7
+   (read-prefix "that has 4 pages, each with text scrolling right, left, down, and right.")
+   'page-8
+   (read-prefix "that has 4 pages, each with text scrolling to the four corners of the page.")
    
-   ; --- Mode Stimuli
-   'mode-1
-   (read-prefix "that has one page with text.")
-   'mode-2
-   (read-prefix "that has two pages one with text moving to the right, the other one with text moving to the left.")
-   'mode-3
-   (read-prefix "that has two pages one with text moving up, the other one with text moving down.")
+   ; --- Sprites Stimuli
+   'sprites-1
+   (read-prefix "that has a sprite and text.")
+   'sprites-2
+   (read-prefix "that has a background image, and a sprite and text moving across the screen.")
+   'sprites-3
+   (read-prefix "that has a background image, and a sprite and text slowly moving to a corner.")
+   'sprites-4
+   (read-prefix "that has text and a sprite in a corner of the page.")
+   'sprites-5
+   (read-prefix "that has text and 4 sprites. Each in a corner of the page.")
+   'sprites-6
+   (read-prefix "that has a background image, customized text, and 2 sprites moving across the screen.")
    ))
--- ts-cutscene/katas/read-code-tips.rkt
+++ ts-cutscene/katas/read-code-tips.rkt
@@ -12,4 +12,18 @@
 
    'hello-world-1
    (list "This is the simplest cutscene possible.")
+
+   'text-1
+   (list "The default font size is 13.")
+   'text-2
+   (list "The font styles are: 'normal, 'italic, and 'slant.")
+   'text-4
+   (list "To change the font family, the font face needs to be set to #f. The font family options are: 'default, 'decorative, 'roman,  'script, 'swiss, 'modern, 'symbol, and 'system")
+   'text-5
+   (list "The font weight can be any value between 100 and 1000.")
+
+   'page-4
+   (list "The background options are: desert-bg, forest-bg, lava-bg, pink-bg, and snow-bg.")
+   'page-5
+   (list "The default page size is 800x600.")
 ))
